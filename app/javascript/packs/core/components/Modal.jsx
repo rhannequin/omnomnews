78074c681b1d6a0eec576a4ae8a704386bb8f1f5
rhannequin <remy.hannequin@gmail.com>
2020-11-23 02:37:36 +0100

    [wip] Add twitter account from UI

---
+++ app/javascript/packs/core/components/Modal.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { DialogOverlay, DialogContent } from "@reach/dialog"
+
+export default function Modal({ariaLabel, onDismiss, children}) {
+  return (
+    <DialogOverlay
+      onDismiss={onDismiss}
+      dangerouslyBypassScrollLock
+      className="overflow-hidden bg-black bg-opacity-75 fixed inset-0 opacity-100"
+      style={{ zIndex: 90 }}
+    >
+      <DialogContent
+        aria-label={ariaLabel}
+        className="rounded-md relative max-w-lg bg-white outline-none overflow-hidden w-11/12 my-20 mx-auto shadow-xl"
+        style={{ maxHeight: "calc(100% - 48px)" }}
+      >
+        {children}
+      </DialogContent>
+    </DialogOverlay>
+  )
+}
--- app/javascript/packs/home/App.jsx
+++ app/javascript/packs/home/App.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react"
 
 import { getGroupedElements } from "../core/api"
 
+import AddTwitterFollowingModal from "./AddTwitterFollowingModal"
 import GroupedElements from "./GroupedElements"
 
 export default function App() {
   const [groupedElements, setGroupedElements] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   useEffect(() => {
     getGroupedElements()
@@ -14,7 +16,21 @@ export default function App() {
       }).catch(err => { console.log(err) })
   }, [])
 
+  const onToggleModal = () => { setIsModalOpen(!isModalOpen) }
+
   return (
-    <GroupedElements groups={groupedElements} />
+    <>
+      <div className="mb-8 max-w-2xl lg:mx-auto space-y-4">
+        <button
+          onClick={onToggleModal}
+          class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          Add Twitter account
+        </button>
+      </div>
+
+      <AddTwitterFollowingModal isOpen={isModalOpen} onClose={onToggleModal} />
+      <GroupedElements groups={groupedElements} />
+    </>
   )
 }
