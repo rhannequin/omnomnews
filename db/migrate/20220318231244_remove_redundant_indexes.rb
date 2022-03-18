32988fb6491f5cd9ef40472fa1f96b644982d76d
Rémy Hannequin <remy.hannequin@gmail.com>
2022-03-19 00:16:06 +0100

    [MIG] Remove redundant indexes

---
+++ db/migrate/20220318231244_remove_redundant_indexes.rb
@@ -0,0 +1,8 @@
+class RemoveRedundantIndexes < ActiveRecord::Migration[7.0]
+  def change
+    remove_index :taggings, name: "index_taggings_on_tag_id"
+    remove_index :taggings, name: "index_taggings_on_taggable_id"
+    remove_index :taggings, name: "index_taggings_on_taggable_type"
+    remove_index :taggings, name: "index_taggings_on_tagger_id"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_18_230833) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_18_231244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -84,14 +84,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_18_230833) do
     t.string "tenant", limit: 128
     t.index ["context"], name: "index_taggings_on_context"
     t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
     t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
     t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
-    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
     t.index ["taggable_type", "taggable_id"], name: "index_taggings_on_taggable_type_and_taggable_id"
-    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
     t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
-    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
     t.index ["tagger_type", "tagger_id"], name: "index_taggings_on_tagger_type_and_tagger_id"
     t.index ["tenant"], name: "index_taggings_on_tenant"
   end
